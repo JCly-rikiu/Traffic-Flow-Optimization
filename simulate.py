845ed90500df725972d116df454d04a4751e92bf
unknown <Rajon010>
2016-06-14 20:06:43 +0800

    simulate.py first commit

---
+++ simulate.py
@@ -0,0 +1,13 @@
+# car.py
+
+from car import Car
+from car import CarMap # merge to one line?
+
+def remove(self, number):
+
+if __name__ == '__main__':
+    cm = CarMap('test')
+    cm.initialCars([(2, 3), (3, 3)])
+    tick = 0
+    while 
+    print(cm.getDirection((4, 4), (8, 4)))
